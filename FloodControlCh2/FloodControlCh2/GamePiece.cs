7f3c77a6570dbcaccb82e803f1e4e7a3b021eb47
Bill Crosbie <crosbie.git@gmail.com>
2015-09-18 08:44:58 -0400

    Set up variables and Properties
    (page 31)

--- FloodControlCh2/FloodControlCh2/GamePiece.cs
+++ FloodControlCh2/FloodControlCh2/GamePiece.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace FloodControlCh2
 {
     class GamePiece
     {
+        public static string[] PieceTypes =
+        {
+                "Left,Right",
+                "Top,Bottom",
+                "Left,Top",
+                "Top,Right",
+                "Right,Bottom",
+                "Bottom,Left",
+                "Empty"
+        };
+
+        public const int PIECE_HEIGHT = 40;
+        public const int PIECE_WIDTH = 40;
+
+        public const int MAX_PLAYABLE_PIECE_INDEX = 5;
+        public const int EMPTY_PIECE_INDEX = 6;
+
+        private const int TEXTURE_OFFSET_X = 1;
+        private const int TEXTURE_OFFSET_Y = 1;
+        private const int TEXTURE_PADDING_X = 1;
+        private const int TEXTURE_PADDING_Y = 1;
+
+        private string pieceType="";
+        private string pieceSuffix = "";
+
+
+        public string PieceType
+        {
+            get { return pieceType; }
+        }
+
+        public string PieceSuffix
+        {
+            get { return pieceSuffix; }
+        }
+
     }
 }
